0513b7e025259db85dda0de2c733635c1601e626
OgGoddy <goddybrown111@gmail.com>
2024-09-09 22:16:19 +0100

    mobile query changes

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ const nav = document.getElementById('navbar');
 
 if (bar) {
     bar.addEventListener('click', () => {
+        console.log('Navbar is open');
         nav.classList.add('active');
     })
 }
 
 if (close) {
-    click.addEventListener('click', () => {
+    close.addEventListener('click', () => {
+        console.log('Nabar is closed');
         nav.classList.remove('active');
     })
 }
+
+// bar.addEventListener('click', () => {
+//     console.log("Nav opened");
+//     nav.classList.add('active');
+// });
+
+// close.addEventListener('click', () => {
+//     console.log("Nav closed");
+//     nav.classList.remove('active');
+// });
